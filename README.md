1a170697835e835f1162085491aa970431c89746
U K I <55081130+uki-hub@users.noreply.github.com>
2020-07-06 21:24:28 +0700

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# live
-for deploying and testing.
---
+++ precache-manifest.acb5cad2bcaf26481d0817b033adc703.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa93503184711fddadd3f6163f686fd6",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e9757ab5da9fdcd77b96",
+    "url": "/react-gh-pages/static/css/main.3268e46f.chunk.css"
+  },
+  {
+    "revision": "9d7c6dda61cd56e6ccf0",
+    "url": "/react-gh-pages/static/js/2.56f68516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.56f68516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9757ab5da9fdcd77b96",
+    "url": "/react-gh-pages/static/js/main.6a1e1aa6.chunk.js"
+  },
+  {
+    "revision": "beec066d49f01b10c6e8",
+    "url": "/react-gh-pages/static/js/runtime-main.b45f0eea.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-gh-pages/static/media/burger.b8503d26.png"
+  }
+]);
