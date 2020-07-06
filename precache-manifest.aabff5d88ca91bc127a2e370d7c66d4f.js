d4a9aafe8335ebbc19c7c1aab7b0e03c7ef529a0
U K I <55081130+uki-hub@users.noreply.github.com>
2020-07-06 21:26:23 +0700

    Updates

---
+++ precache-manifest.aabff5d88ca91bc127a2e370d7c66d4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0545c25e45a6f706eb635f04c538d8a",
+    "url": "/live-demo/index.html"
+  },
+  {
+    "revision": "77bbbe9d7b80d5c4afa2",
+    "url": "/live-demo/static/css/main.3268e46f.chunk.css"
+  },
+  {
+    "revision": "9d7c6dda61cd56e6ccf0",
+    "url": "/live-demo/static/js/2.56f68516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/live-demo/static/js/2.56f68516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77bbbe9d7b80d5c4afa2",
+    "url": "/live-demo/static/js/main.c4af3998.chunk.js"
+  },
+  {
+    "revision": "494256d0dad9bcf07a1e",
+    "url": "/live-demo/static/js/runtime-main.8eb393ba.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/live-demo/static/media/burger.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.acb5cad2bcaf26481d0817b033adc703.js"
+  "/live-demo/precache-manifest.aabff5d88ca91bc127a2e370d7c66d4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/live-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
